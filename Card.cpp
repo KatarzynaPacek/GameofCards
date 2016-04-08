6fbf64e61ce681e3759672956f9e4d80d5902a41
Katarzyna Pacek <205832@student.pwr.edu.pl>
2016-04-08 12:19:57 +0200

    Git init

---
+++ Card.cpp
@@ -0,0 +1,36 @@
+# include "Card.h"
+#include <string>
+
+using namespace std;
+
+int Card::counter =0;
+
+Card::Card()
+{
+
+	figure = counter % 13;
+	colour = counter / 13;
+	counter ++;
+}
+
+result Card::compare(Card *)
+{
+
+	return win;
+
+
+}
+
+string Card::description()
+{
+
+	static char colours[]= "KPTR";
+	static char figures[] = "234567890JQKA";
+	return string(1 , colours[colour])+string(1 , figures[figure]);
+
+
+
+
+
+
+}
